464a930785159038b5b19e775d5cd478802b026f
Ryan Kwon <kznm.develop@gmail.com>
2022-03-21 11:28:38 +0900

    Create 2022-03-21 Homework.py

---
+++ 2022-03-21 Homework.py
@@ -0,0 +1,42 @@
+#LAB_1
+x = 10
+print(x)
+x = x * x
+print(x)
+
+#LAB_2
+name = "권동한"
+print("안녕하세요. 제 이름은 ", name, "입니다.")
+print("안녕하세요. 제 이름은 " + name + "입니다.")
+
+#LAB_3
+a = 5
+b = float(2) # 2.0
+c = a // b
+print("a:", a)
+print("b:", b)
+print("c:", c)
+
+#HW_1
+x = 2
+y = 4
+a = x + y
+b = x - y
+c = x / y
+d = x // y
+e = x % y
+f = x ** y
+print(a, b, c, d, e, f)
+
+#HW_2
+x = 5+5
+print(x)    # 정수 5 + 5 = 10
+x = '5'+'5'
+print(x)    # 문자열 '5'와 '5'를 연결하여 '55'
+x = '5+5'
+print(x)    # 문자열 '5+5'를 그대로 출력
+
+#HW_3
+a = '25'
+b = '20'
+print(int(a) + int(b))

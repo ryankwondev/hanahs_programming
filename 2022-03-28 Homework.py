2b3abb59a59b05b65216315f3ec0faaa50e4005d
Ryan Kwon <kznm.develop@gmail.com>
2022-03-28 11:37:40 +0900

    Create 2022-03-28 Homework.py

---
+++ 2022-03-28 Homework.py
@@ -0,0 +1,14 @@
+# 권동한_HW1
+slang = 'Oh My God'
+
+print(slang[0] + slang[3] + slang[6])
+
+# 권동한_HW2
+r = int(input("반지름 : "))
+pi = 3.141592
+
+c_len = 2 * pi * r
+c_area = pi * (r ** 2)
+
+print("원의 둘레는", c_len)
+print("원의 넓이는", c_area)

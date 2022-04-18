a44bca6eee5eda6d8cfbf3c231c3eea70823f8cf
Ryan Kwon <kznm.develop@gmail.com>
2022-04-18 11:28:17 +0900

    Create 2022-04-18 Homework.py

---
+++ 2022-04-18 Homework.py
@@ -0,0 +1,15 @@
+# 권동한_HW1
+N = int(input("입력하라: "))
+
+for i in range(2, N, 2):
+    print(i)
+
+# 권동한_HW2
+inp = -1
+sum = 0
+
+while inp != 0:
+    inp = int(input("입력해라: "))
+    sum += inp
+
+print(sum)
